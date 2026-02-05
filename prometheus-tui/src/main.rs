fca45cf600876c803d91377a2a9eacd077ea0ee2
Aryan <aryansharma24112003@gmail.com>
2026-02-05 22:31:45 +0530

    chore: reset to v1.0.0 for launch

--- prometheus-tui/src/main.rs
+++ prometheus-tui/src/main.rs
@@ -1747,7 +1747,7 @@ fn render_home(frame: &mut ratatui::Frame, area: Rect, state: &AppState) {
     lines.push(Line::from(vec![
         Span::styled("☕ ", Style::default().fg(colors::ACCENT_YELLOW)),
         Span::styled("Support this project: ", Style::default().fg(colors::TEXT_MUTED)),
-        Span::styled("github.com/sponsors/Aryan-Protein-Vala", Style::default().fg(colors::ACCENT_CYAN)),
+        Span::styled("github.com/Aryan-Protein-Vala/Prometheus", Style::default().fg(colors::ACCENT_CYAN)),
     ]));
     lines.push(Line::from(vec![
         Span::styled("📢 ", Style::default().fg(colors::ACCENT_PURPLE)),
